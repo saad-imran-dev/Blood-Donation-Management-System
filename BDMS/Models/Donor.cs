7b257dc3ea99192e9113447f72c6f3c77a7d69fe
AE186 <saad.imran.vohra@gmail.com>
2022-11-29 00:17:33 -0800

    Cnic updated from int to string

--- BDMS/Models/Donor.cs
+++ BDMS/Models/Donor.cs
@@ -6,17 +6,18 @@ namespace BDMS.Models
     public class Donor
     {
         public int Id { get; set; }
-        public int Cnic { get; set; }
         [Required]
-        public string FirstName { get; set; }
+        public string? Cnic { get; set; }
         [Required]
-        public string LastName { get; set; }
+        public string? FirstName { get; set; }
         [Required]
-        public string Address { get; set; }
+        public string? LastName { get; set; }
         [Required]
-        public string Email { get; set; }
+        public string? Address { get; set; }
         [Required]
-        public string Password { get; set; }
+        public string? Email { get; set; }
+        [Required]
+        public string? Password { get; set; }
 
         [ForeignKey("Area")]
         public int AreaCode { get; set; }
