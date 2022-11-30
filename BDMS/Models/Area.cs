b40d8baef6408a21224ea760ba17beb92da29e25
Ammar <ammarsiddiqui247@gmail.com>
2022-11-30 19:26:16 +0500

    Organization View and Controller Created and Razor View Upgraded

--- BDMS/Models/Area.cs
+++ BDMS/Models/Area.cs
@@ -6,7 +6,7 @@ namespace BDMS.Models
     {
         public int Id { get; set; }
         [Required] 
-        public string? Name { get; set; }
+        public string Name { get; set; }
         [Required]
         public string? City { get; set; }
         [Required]
